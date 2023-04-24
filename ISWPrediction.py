f98a92aab29e9b65640abb246744a1c128a2be5d
yuliastoliaruk <uliastoliaruk@gmail.com>
2023-04-24 21:31:23 +0300

    Updated make vector from ISW report for prediction

--- ISWPrediction.py
+++ ISWPrediction.py
@@ -1,16 +1,14 @@
 import pandas as pd
-import IswPrediction_1
+import save_page
 import IswPrediction_2
 import IswPrediction_3
-from datetime import datetime, date
-
-#data after first step
-INPUT_FOLDER = "prediction"
-DATA_FILE = "pred_isw_v1.csv"
+from bs4 import BeautifulSoup
+from datetime import datetime, timedelta, date
 
 #dictionary from documents
 INPUT_FOLDER_2 = "data"
 DATA_FILE_2 = "parsed_isw.csv"
+df_for_dict = pd.read_csv(f"{INPUT_FOLDER_2}/{DATA_FILE_2}", sep=";").fillna(" ")
 
 def take_vector_from_isw(date_input):
     # Convert the string date into a datetime object using strptime()
@@ -24,22 +22,34 @@ def take_vector_from_isw(date_input):
         # Set data_object to today's date
         date_object = today
 
-    # Check if the page is already download and save it if nessesary. Then save dataframe with html body
-    IswPrediction_1.firstSt(date_object, INPUT_FOLDER, DATA_FILE)
+    # Take date for yesterday
+    date_object = date_object - timedelta(days=1)
 
-    df = pd.read_csv(f"{INPUT_FOLDER}/{DATA_FILE}", sep=";").fillna(" ")
-    df_for_dict = pd.read_csv(f"{INPUT_FOLDER_2}/{DATA_FILE_2}", sep=";").fillna(" ")
+    # Take content from page
+    content = save_page.take_page(date_object)
+
+    all_data = []
+    # Extract main text from ISW report
+    parsed_html = BeautifulSoup(content)
+    text_main = parsed_html.body.find("div", attrs={
+        'class': "field field-name-body field-type-text-with-summary field-label-hidden"})
+    d = {
+        "date": date_object,
+        "main_text": str(text_main)
+    }
+    # Create data frame with date and main text for future processing
+    all_data.append(d)
+    df = pd.DataFrame.from_dict(all_data)
 
     # Clean text
     df_st2 = IswPrediction_2.secondSt(df)
 
     # Creat vector from text
     keywords = IswPrediction_3.thirdSt(df_st2, df_for_dict)
-
     return keywords
 
 #test
-#date_input = "23.04.2023"
-#vector = take_vector_from_isw(date_input)
-#for i in vector:
+#date_input = "28.04.2023"
+#keywords = take_vector_from_isw(date_input)
+#for i in keywords:
 #    print(i)
