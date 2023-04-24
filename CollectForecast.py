c3e84839448decfa74a1bc79d6de0ad17b332d22
Evgen Vinokur <vinokur.eg@gmail.com>
2023-04-25 02:17:04 +0300

    WeatherScript.py now includes avg day data, Made CollectForecast.py dataframe match the trained data with daily_* and hour_* values

--- CollectForecast.py
+++ CollectForecast.py
@@ -1,10 +1,29 @@
+import pickle
+import numpy as np
 import pandas as pd
 from WeatherScript import *
 
 # ENTER INPUT TO regions.csv HERE
 PATH_TO_REGIONS_ID = ""
+# ENTER INPUT TO .pkl of hour_conditions encoder HERE
+PATH_TO_HOUR_CONDITIONS_ENCODER = ""
 
 regions = pd.read_csv(f"{PATH_TO_REGIONS_ID}", sep=",")
+
+
+def encode_precip_type(precip):
+    types = [
+        ['freezingrain'],
+        ['ice'],
+        ['rain', 'snow'],
+        ['rain'],
+        ['snow'],
+        np.nan
+    ]
+    for type in types:
+        if precip == type:
+            return types.index(type)
+    return types.index(np.nan)
 
 
 # Function returns a dataframe with hourly forecast for the weather in all regions
@@ -29,8 +48,52 @@ def getNewForecast():
 
         region_df = pd.DataFrame.from_dict(region_hours)
         region_df["region_id"] = regions["region_id"][region_id - 1]
-        region_df["region"] = regions["region"][region_id - 1]
         weather_df = pd.concat([weather_df, region_df])
+
+    weather_exclude = [
+        "day_datetime",
+        "day_feelslikemax",
+        "day_feelslikemin",
+        "day_sunriseEpoch",
+        "day_sunsetEpoch",
+        "day_description",
+        "day_feelslike",
+        "day_precipprob",
+        "day_snow",
+        "day_snowdepth",
+        "day_windgust",
+        "day_windspeed",
+        "day_winddir",
+        "day_pressure",
+        "day_cloudcover",
+        "day_visibility",
+        "day_severerisk",
+        "day_conditions",
+        "day_icon",
+        "day_source",
+        "day_preciptype",
+        "day_stations",
+        "hour_datetime",
+        "hour_icon",
+        "hour_source",
+        "hour_stations",
+        "hour_feelslike"
+    ]
+    weather_df = weather_df.drop(weather_exclude, axis=1)
+    with open(f"{PATH_TO_HOUR_CONDITIONS_ENCODER}", "rb") as encoder_reader:
+        label_encoder = pickle.load(encoder_reader)
+        weather_df["hour_conditions"] = label_encoder.transform(weather_df["hour_conditions"])
+
+    weather_df["hour_preciptype"].fillna(value=np.nan, inplace=True)
+    weather_df["hour_preciptype"] = weather_df["hour_preciptype"]\
+        .apply(lambda x: encode_precip_type(x))
+
+    weather_df["hour_solarenergy"] = weather_df["hour_solarenergy"] \
+        .apply(lambda x: 0 if x != x else x)
+    weather_df["day_sunrise"] = weather_df["day_sunrise"]. \
+        apply(lambda x: datetime.strptime(x, "%H:%M:%S").hour * 60 + datetime.strptime(x, "%H:%M:%S").minute)
+    weather_df["day_sunset"] = weather_df["day_sunset"]. \
+        apply(lambda x: datetime.strptime(x, "%H:%M:%S").hour * 60 + datetime.strptime(x, "%H:%M:%S").minute)
     return weather_df
 
 
--- WeatherScript.py
+++ WeatherScript.py
@@ -29,25 +29,31 @@ def generate_weather(city: str, date: str):
 
 # By entering date it will get info about weather for the full day
 # If the date was not entered, it will get info for the next 12 hours
-def getWeatherHours (city: str, date: str):
-    json = generate_weather(city, date)['days'][0]['hours']
+def getWeatherHours(city: str, date: str):
+    json = generate_weather(city, date)['days'][0]
+    hours_json = json.pop('hours')
+    json = {"day_" + k:v for k,v in json.items()}
     hours = []
     if date:
-        for hour in json:
+        for hour in hours_json:
+            hour = json | {"hour_" + k: v for k, v in hour.items()}
             hours.append(hour)
     else:
         hour_now = datetime.now().hour
         diff = 0
         if 0 < 24 - hour_now < 12:
             diff = 12 - (24 - hour_now)
-            json2 = generate_weather(city, (datetime.today() + timedelta(days=1)).strftime("%Y-%m-%d"))['days'][0][
-                'hours']
-        for hour in json:
-            if datetime.strptime(hour['datetime'], "%H:%M:%S").hour >= hour_now:
+            json2 = generate_weather(city, (datetime.today() + timedelta(days=1)).strftime("%Y-%m-%d"))['days'][0]
+            hours_json2 = json2.pop('hours')
+            json2 = {"day_" + k: v for k, v in json2.items()}
+        for hour in hours_json:
+            hour = json | {"hour_" + k: v for k, v in hour.items()}
+            if datetime.strptime(hour['hour_datetime'], "%H:%M:%S").hour >= hour_now:
                 hours.append(hour)
         if diff:
-            for hour in json2:
-                if datetime.strptime(hour['datetime'], "%H:%M:%S").hour < diff:
+            for hour in hours_json2:
+                hour = json2 | {"hour_" + k: v for k, v in hour.items()}
+                if datetime.strptime(hour['hour_datetime'], "%H:%M:%S").hour < diff:
                     hours.append(hour)
                 else:
                     break
