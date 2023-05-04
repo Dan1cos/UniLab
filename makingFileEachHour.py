79c72d420621878fe57f72b94b1f03a94f2e1895
Artem Shakirov <63858987+Dan1cos@users.noreply.github.com>
2023-05-04 23:56:08 +0200

    Added comments

--- makingFileEachHour.py
+++ makingFileEachHour.py
@@ -13,19 +13,25 @@ OUTPUT_FOLDER = "data"
 OUTPUT_CSV = "predictedAlarms.csv"
 OUTPUT_LOG = "timestamps.log"
 
+#checking if the log file is present, logging first run of the script
 if not exists(f"{OUTPUT_FOLDER}/{OUTPUT_LOG}"):
     logging.basicConfig(filename=f"{OUTPUT_FOLDER}/{OUTPUT_LOG}", format='%(asctime)s %(message)s')
     logging.warning('')
 
+#loading model
 file = open(f"{PATH_TO_MODEL}", 'rb')
 clf = pickle.load(file)
 file.close()
 
+#creading dataframe for today 
 df = makingMergedForEachHour.createDataSet(datetime.today().strftime("%d.%m.%Y"))
+
+#making prediction in model
 pred = clf.predict(df)
 received = pd.DataFrame(pred, columns=["prediction"])
-regions_df = pd.read_csv(f"{PATH_TO_REGIONS}", sep=",")
 
+#editing received information
+regions_df = pd.read_csv(f"{PATH_TO_REGIONS}", sep=",")
 received["region_id"] = df["region_id"]
 received["hour_datetimeEpoch"] = df["hour_datetimeEpoch"]
 received["hour_datetimeEpoch"] = received["hour_datetimeEpoch"].apply(
@@ -34,7 +40,9 @@ received = received.rename(columns={"hour_datetimeEpoch": "time"})
 received = received.merge(regions_df[["center_city_en", "region_id"]], how="left", left_on=["region_id"],
                           right_on=["region_id"])
 
+#exporting to the csv file
 received.to_csv(f"{OUTPUT_FOLDER}/{OUTPUT_CSV}", mode='a', sep=";", index=False)
 
+#adding log
 logging.basicConfig(filename=f"{OUTPUT_FOLDER}/{OUTPUT_LOG}", format='%(asctime)s %(message)s')
 logging.warning('')
