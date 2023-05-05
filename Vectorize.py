11b47c7700bb7c1862a477be41733c1e22880227
Artem Shakirov <63858987+Dan1cos@users.noreply.github.com>
2023-05-05 19:46:19 +0200

    Updated script

--- Vectorize.py
+++ Vectorize.py
@@ -4,63 +4,52 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 import pickle
 
-INPUT_FOLDER = "isw_parsed_2"
-DATA_FILE = "parsed_isw.csv"
+def thirdSt(df_for_predict, df_for_dict):
+    docs = df_for_dict['report_text'].tolist()
 
-OUTPUT_FOLDER = "isw_parsed_3"
-OUTPUT_DATA_FILE = "vector.csv"
+    cv = CountVectorizer(max_df=0.98, min_df=2)
+    word_count_vector = cv.fit_transform(docs)
 
-df = pd.read_csv(f"{INPUT_FOLDER}/{DATA_FILE}", sep=";").fillna(" ")
+    with open("isw_parsed_3/count_vectorizer.pkl", "wb") as handle:
+        pickle.dump(cv, handle)
 
-docs = df['report_text'].tolist()
+    tfidf_transformer = TfidfTransformer()
+    tfidf_transformer.fit(word_count_vector)
 
-cv = CountVectorizer(max_df=0.98, min_df=2)
-word_count_vector = cv.fit_transform(docs)
+    df_idf = pd.DataFrame(tfidf_transformer.idf_, index=cv.get_feature_names_out(), columns=["idf_weights"])
+    df_idf.sort_values(by=["idf_weights"])
 
-with open("isw_parsed_3/count_vectorizer.pkl", "wb") as handle:
-    pickle.dump(cv, handle)
+    tf_idf_vector = tfidf_transformer.transform(word_count_vector)
 
-tfidf_transformer = TfidfTransformer()
-tfidf_transformer.fit(word_count_vector)
+    def sort_coo(coo_matrix):
+        tuples = zip(coo_matrix.col, coo_matrix.data)
+        return sorted(tuples, key=lambda x:(x[1], x[0]), reverse=True)
 
-df_idf = pd.DataFrame(tfidf_transformer.idf_, index=cv.get_feature_names_out(), columns=["idf_weights"])
-df_idf.sort_values(by=["idf_weights"])
+    def extract_topn_from_vector(feature_names, sorted_items, topn=10):
+        sorted_items = sorted_items[:topn]
 
-tf_idf_vector = tfidf_transformer.transform(word_count_vector)
+        score_vals = []
+        feature_vals = []
 
-def sort_coo(coo_matrix):
-    tuples = zip(coo_matrix.col, coo_matrix.data)
-    return sorted(tuples, key=lambda x:(x[1], x[0]), reverse=True)
+        for idx, score in sorted_items:
+            score_vals.append(round(score,3))
+            feature_vals.append(feature_names[idx])
 
-def extract_topn_from_vector(feature_names, sorted_items, topn=10):
-    sorted_items = sorted_items[:topn]
+        results = {}
+        for idx in range(len(feature_vals)):
+            results[feature_vals[idx]]=score_vals[idx]
+        return results
 
-    score_vals = []
-    feature_vals = []
+    def convert_to_vector(doc):
+        feature_names = cv.get_feature_names_out()
+        top_n = 100
+        tf_idf_vector = tfidf_transformer.transform(cv.transform([doc]))
 
-    for idx, score in sorted_items:
-        score_vals.append(round(score,3))
-        feature_vals.append(feature_names[idx])
+        sorted_items = sort_coo(tf_idf_vector.tocoo())
 
-    results = {}
-    for idx in range(len(feature_vals)):
-        results[feature_vals[idx]]=score_vals[idx]
-    return results
+        keywords = extract_topn_from_vector(feature_names,sorted_items, top_n)
 
-def convert_to_vector(doc):
-    feature_names = cv.get_feature_names_out()
-    top_n = 100
-    tf_idf_vector = tfidf_transformer.transform(cv.transform([doc]))
+        return keywords
 
-    sorted_items = sort_coo(tf_idf_vector.tocoo())
-
-    keywords = extract_topn_from_vector(feature_names,sorted_items, top_n)
-
+    keywords = df_for_predict['report_text'].apply(lambda x: convert_to_vector(x))
     return keywords
-
-
-df['keywords'] = df['report_text'].apply(lambda x: convert_to_vector(x))
-
-new_df = df[['date','url','keywords']].copy()
-
-new_df.to_csv(f"{OUTPUT_FOLDER}/{OUTPUT_DATA_FILE}", sep=";", index=False)
