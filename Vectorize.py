cebce93a7afeeb157b1652e3c736b6374b6a938f
Artem Shakirov <63858987+Dan1cos@users.noreply.github.com>
2023-05-05 19:50:09 +0200

    Added comments

--- Vectorize.py
+++ Vectorize.py
@@ -7,12 +7,11 @@ import pickle
 def thirdSt(df_for_predict, df_for_dict):
     docs = df_for_dict['report_text'].tolist()
 
+    #using CountVectorizer to count each number word
     cv = CountVectorizer(max_df=0.98, min_df=2)
     word_count_vector = cv.fit_transform(docs)
 
-    with open("isw_parsed_3/count_vectorizer.pkl", "wb") as handle:
-        pickle.dump(cv, handle)
-
+    #creating TfidfTransformer for further convertation of counted words to tfidf
     tfidf_transformer = TfidfTransformer()
     tfidf_transformer.fit(word_count_vector)
 
@@ -21,10 +20,12 @@ def thirdSt(df_for_predict, df_for_dict):
 
     tf_idf_vector = tfidf_transformer.transform(word_count_vector)
 
+    #sorting function
     def sort_coo(coo_matrix):
         tuples = zip(coo_matrix.col, coo_matrix.data)
         return sorted(tuples, key=lambda x:(x[1], x[0]), reverse=True)
 
+    #taking n number of top rows
     def extract_topn_from_vector(feature_names, sorted_items, topn=10):
         sorted_items = sorted_items[:topn]
 
@@ -40,6 +41,7 @@ def thirdSt(df_for_predict, df_for_dict):
             results[feature_vals[idx]]=score_vals[idx]
         return results
 
+    #convrting words into tfidf vector
     def convert_to_vector(doc):
         feature_names = cv.get_feature_names_out()
         top_n = 100
@@ -52,4 +54,5 @@ def thirdSt(df_for_predict, df_for_dict):
         return keywords
 
     keywords = df_for_predict['report_text'].apply(lambda x: convert_to_vector(x))
+    
     return keywords
