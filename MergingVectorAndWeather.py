39c24ded77e38c07d1b0836fa00350924a8860b7
Швець 0лексій <shvec.01eksij@gmail.com>
2023-05-05 22:05:56 +0300

    cleanupcommit_4

--- MergingVectorAndWeather.py
+++ MergingVectorAndWeather.py
@@ -3,22 +3,28 @@ import json
 import IswPrediction_final
 import CollectForecast
 import numpy as np
+from datetime import datetime
 
+VECTOR_PATH = 'data/emptyVector.csv'
 
-VECTOR_PATH = 'D:/PythonLab/UniLab/data/vector_v2_dates.csv'
 
 def createDataSet(date_input):
     vector1 = IswPrediction_final.take_vector_from_isw(date_input)
     vector1 = {k: v for d in vector1.values() for k, v in d.items()}
-    
+
     vector2 = pd.read_csv(VECTOR_PATH, sep=';')
     vector2.loc[0] = {}
     for i in vector2.columns:
         if i in vector1:
             vector2[i] = vector1[i]
     vector2 = vector2.fillna(0)
-    
+
     df_weather = CollectForecast.getNewForecast()
-    final_merged_set = df_weather.merge(vector2)
-    
+    df_weather['just_to_join'] = "1"
+    vector2["just_to_join"] = "1"
+    final_merged_set = df_weather.merge(vector2, how="left", left_on="just_to_join", right_on="just_to_join")
+    final_merged_set = final_merged_set.drop(["just_to_join", "Unnamed: 0", "date"], axis=1)
+    final_merged_set = final_merged_set.rename(columns = {"city":"city_y", "region":"region_y"})
+
+
     return final_merged_set
